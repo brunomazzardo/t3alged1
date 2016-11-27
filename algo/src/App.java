01e1470dfa6d691c4b3725cc96f41a7c635c1f5d
Bruno <Bruno@Bruno-PC>
2016-11-27 01:48:59 -0200

    2

--- algo/src/App.java
+++ algo/src/App.java
@@ -3,10 +3,8 @@ import java.util.ArrayList;
 
 public class App {
 
-    public static void main(String[] args) {
-    
-     
-        
-    }
-    
+	public static void main(String[] args) {
+
+	}
+
 }
