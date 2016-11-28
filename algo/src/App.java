c2984f819100c775cbac78c4c9b626c342a2a60b
Lucas Demoliner <Lucas.demoliner@acad.pucrs.br>
2016-11-28 15:46:02 -0200

    tst

--- algo/src/App.java
+++ algo/src/App.java
@@ -1,19 +1,24 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        try{
 
-		String destino = args[0];
-		leituraDosLivros teste = new leituraDosLivros(destino);
-		formataSumario t1 = new formataSumario();
-		
-		t1.gravaNoArquivo(args[1], (teste.leTudo().positionsPre()));
-		System.out.println("Gerando Sumário... ok");
-		System.out.println("Imprimindo livro para o arquivo " + args[1]);
-	
+        String destino = args[0];
+        leituraDosLivros teste = new leituraDosLivros(destino);
+        formataSumario t1 = new formataSumario();
 
-	}
+        t1.gravaNoArquivo(args[1], (teste.leTudo().positionsPre()));
+        System.out.println("Gerando Sumário... ok");
+        System.out.println("Imprimindo livro para o arquivo " + args[1]);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Deve ser iniciado pelo terminal");
+        }catch (Exception s){
+            System.out.println("Erro na execução");
+        }
+
+
+    }
 }
